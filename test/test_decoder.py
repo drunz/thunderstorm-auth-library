4fdf4a4a22e92291a359fddeaec10a23c558dfba
Rob Young <rob@robyoung.digital>
2017-10-19 15:07:54 +0100

    [fix] add leeway to the leeway
    The tests that test the leeway argument set it to 3600s (1 hour).
    The expired token fixture is set as expired 1 hours ago.
    This means that if the tests are running on a slightly slow machine then
    they can fail sometimes. This adds a bit of leeway to the leeway
    argument.

--- test/test_decoder.py
+++ test/test_decoder.py
@@ -20,10 +20,10 @@ def test_get_decoded_token_raises_if_jwt_expired(expired_token, jwk_set):
 
 def test_get_decoded_token_does_not_raise_if_jwt_expired_but_leeway_is_set(
         expired_token, jwk_set):
-    assert decode_token(expired_token, jwk_set, leeway=3600)
+    assert decode_token(expired_token, jwk_set, leeway=3605)
 
 
 def test_get_decoded_token_with_jwk_set_raises_broken_token_error_if_token_is_malformed(
         invalid_token, jwk_set):
     with pytest.raises(BrokenTokenError):
-        decode_token(invalid_token, jwk_set, leeway=3600)
+        decode_token(invalid_token, jwk_set, leeway=3605)
