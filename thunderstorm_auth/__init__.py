1de96d796340e0769771c8dafba7f5bd0afa0de8
Rob Young <rob@robyoung.digital>
2017-10-19 12:41:08 +0100

    [util] Add a util function to load jwks from file

--- thunderstorm_auth/__init__.py
+++ thunderstorm_auth/__init__.py
@@ -1,3 +1,6 @@
+from .utils import load_jwks_from_file
+
+
 __title__ = 'thunderstorm-auth-lib'
 __version__ = '0.0.3'
 
@@ -5,3 +8,5 @@ __version__ = '0.0.3'
 TOKEN_HEADER = 'X-Thunderstorm-Key'
 
 DEFAULT_LEEWAY = 0
+
+__all__ = ['load_jwks_from_file']
