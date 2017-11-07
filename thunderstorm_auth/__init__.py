987bacdeeb28e85db566ae2179d5992842f69494
alexanderson <alexander.anderson@artsalliancemedia.com>
2017-11-07 15:59:37 +0000

    bump version

--- thunderstorm_auth/__init__.py
+++ thunderstorm_auth/__init__.py
@@ -2,7 +2,7 @@ from thunderstorm_auth.utils import load_jwks_from_file
 
 
 __title__ = 'thunderstorm-auth-lib'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 TOKEN_HEADER = 'X-Thunderstorm-Key'
