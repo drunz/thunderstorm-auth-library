eaff031bb47c654723fcadb07ede720f60f3346f
alexanderson <alexander.anderson@artsalliancemedia.com>
2017-11-07 16:27:37 +0000

    remove import from __init__ to stop pip install from src failing

--- thunderstorm_auth/__init__.py
+++ thunderstorm_auth/__init__.py
@@ -1,6 +1,3 @@
-from thunderstorm_auth.utils import load_jwks_from_file
-
-
 __title__ = 'thunderstorm-auth-lib'
 __version__ = '0.0.6'
 
@@ -8,5 +5,3 @@ __version__ = '0.0.6'
 TOKEN_HEADER = 'X-Thunderstorm-Key'
 
 DEFAULT_LEEWAY = 0
-
-__all__ = ['load_jwks_from_file']
