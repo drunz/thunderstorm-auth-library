0dd9c51bbfe793debcd71a4a5d61c4be6380ecdc
alexanderson <alexander.anderson@artsalliancemedia.com>
2017-11-21 17:03:16 +0000

    Use tox container to run multi-platform tests

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM themattrix/tox
+
+COPY thunderstorm_auth /src/thunderstorm_auth/
+COPY test /src/test/
---
+++ install-prereqs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script is used by tox container to install requirements
+
+apt-get update \
+    && apt-get -y --no-install-recommends install build-essential libffi-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
