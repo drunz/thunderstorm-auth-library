ff41b54b683f39a128f65ff19ef7be3874d9c17f
Rob Young <rob@robyoung.digital>
2018-01-11 15:25:18 +0000

    [feature] add request-id requests wrapper
    Add a module that wraps requests and adds the TS-Request-ID header to
    each call. This ensures request IDs are propagated across service calls.

---
+++ test/logging/test_requests.py
@@ -0,0 +1,38 @@
+from unittest import mock
+
+from thunderstorm_auth.logging import requests
+
+
+@mock.patch('thunderstorm_auth.logging.requests._get')
+@mock.patch('thunderstorm_auth.logging.requests._get_request_id')
+def test_get(mock_get_request_id, mock_get):
+    mock_get_request_id.return_value = 'request-id'
+
+    requests.get('/')
+
+    mock_get.assert_called_with('/', headers={'TS-Request-ID': 'request-id'})
+
+
+@mock.patch('thunderstorm_auth.logging.requests._get')
+@mock.patch('thunderstorm_auth.logging.requests._get_request_id')
+def test_get_with_headers(mock_get_request_id, mock_get):
+    mock_get_request_id.return_value = 'request-id'
+
+    requests.get('/', headers={'foo': 'bar'})
+
+    mock_get.assert_called_with('/', headers={
+        'TS-Request-ID': 'request-id',
+        'foo': 'bar',
+    })
+
+
+@mock.patch('thunderstorm_auth.logging.requests._get')
+@mock.patch('thunderstorm_auth.logging.requests._get_request_id')
+def test_get_with_request_id(mock_get_request_id, mock_get):
+    mock_get_request_id.return_value = 'request-id'
+
+    requests.get('/', headers={'TS-Request-ID': 'my-id'})
+
+    mock_get.assert_called_with('/', headers={
+        'TS-Request-ID': 'my-id',
+    })
