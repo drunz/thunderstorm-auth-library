66abf02a274039951a3bafc670fb2086ede7b0c9
Rob Young <rob+github@robyoung.digital>
2018-02-07 17:37:06 +0000

    [feature] Add permission model
    Add the Permission model and a function that uses this to create a
    concrete Permission model that is based off an apps SQLAlchemy
    declarative base.

--- thunderstorm_auth/permissions.py
+++ thunderstorm_auth/permissions.py
@@ -1,4 +1,8 @@
 from collections.abc import Mapping
+from uuid import uuid4
+
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
 
 from thunderstorm_auth.exceptions import (
     InsufficientPermissions, BrokenTokenError
@@ -24,3 +28,37 @@ def validate_permission(token_data, service_name, permission):
         raise BrokenTokenError()
     elif permission not in token_data['permissions'].get(service_name, []):
         raise InsufficientPermissions()
+
+
+def create_permission_model(base):
+    """Create an SQLAlchemy Permission
+
+    This is intended to be used in apps that have permissions.
+
+    Args:
+        base (Base): Declarative base of database schema to add model to.
+
+    Returns:
+        SQLAlchemy Permission model
+    """
+
+    return type(
+        'Permission',
+        (base,),
+        {
+            key: value for key, value in Permission.__dict__.items()
+            if not key.startswith('__') or key == '__tablename__'
+        }
+    )
+
+
+class Permission:
+    __tablename__ = 'permission'
+
+    uuid = Column(
+        UUID(as_uuid=True), primary_key=True, default=lambda: str(uuid4())
+    )
+    service_name = Column(String(255), nullable=False)
+    permission = Column(String(255), nullable=False, unique=True)
+    is_deleted = Column(Boolean(), nullable=False, default=False)
+    is_sent = Column(Boolean(), nullable=False, default=False)
